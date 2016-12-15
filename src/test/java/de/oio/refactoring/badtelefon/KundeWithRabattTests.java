12c1f12246c493ce3607ba6d9ca5c589a6a8e465
Falk Sippach <falk@jug-da.de>
2016-12-15 23:51:26 +0100

    Integration new feature in legacy code

--- src/test/java/de/oio/refactoring/badtelefon/KundeWithRabattTests.java
+++ src/test/java/de/oio/refactoring/badtelefon/KundeWithRabattTests.java
@@ -22,4 +22,19 @@ public class KundeWithRabattTests {
 		assertEquals(1.791, kunde.getGebuehr(), 0.001);
 	}	
 
+	@Test
+	public void testKundeMit10ProzentRabattAndSaveMock() throws Exception {
+		createKundeWithMockedSave(1.791).account(2, 12, 30);
+	}
+
+	private Kunde createKundeWithMockedSave(double erwarteterPreis) {
+		Kunde kunde = new Kunde(Tarif.PRIVAT, new Rabatt(10)) {
+			@Override
+			protected void saveCall(int minuten, int stunde, int minute, double preis) {
+				assertEquals(erwarteterPreis, preis, 0.001);
+			}
+		};
+		return kunde;
+	}
+
 }
